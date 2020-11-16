dcfe3b97a6a568bcb6a74515e550e1f84858d91e
Indu Chouhan <cindu2598@gmail.com>
2020-11-16 18:55:02 +0530

    First React project

---
+++ myfirstproject/src/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// ----------------------------------------------------------------------------------
+// ------                  JSX Attribute in React JS                         --------
+// ----------------------------------------------------------------------------------
+
+
+
+// ----------------------------------------------------------------------------------
+// ------                        JSX Challenge 2                             --------
+// ----------------------------------------------------------------------------------
+
+// const name = "indu"
+// let date = new Date()
+// let date1 = date.toLocaleDateString()
+// let time = date.toLocaleTimeString()
+// ReactDOM.render(
+//     <>
+//     <h1>
+//         {`my name is ${name}`}
+//     </h1>
+//     <p>
+//     {`Today date is ${date1}`}
+//     </p>
+//     <p>
+//    { `Current Time is ${time}`}
+//     </p>
+//     </>,
+//     document.getElementById('root')
+// )
+
+
+// ----------------------------------------------------------------------------------
+// ------                        Template Literals                           --------
+// ----------------------------------------------------------------------------------
+
+// const fname = "indu";
+// const lname = "chouhan soam"
+
+// ReactDOM.render(
+//   <>
+//     <h1>
+//       My name is {fname} {lname}
+//     </h1>
+//     <p>
+//       My name is {fname+lname} : Names are concatenated without space
+//     </p>
+//     <p>
+//       {`My first  and last name is ${fname} ${lname}`}
+//     </p>
+//   </>,
+//   document.getElementById("root")
+// )
+
+
+// ------------------------------javascript in Html------------------------------------
+// const fname= 'indu chouhan'
+// ReactDOM.render(
+//   <>
+//   <h1>My name is {fname}</h1>
+//   <p> my lucky number is {2+3}</p>
+//   <p> my 2nd lucky number is {2*3}</p>
+//   <p> my 2nd lucky number is {Math.random()}</p>
+//   </>,
+//   document.getElementById('root')
+// );
+
+
+
